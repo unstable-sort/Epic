d13fb0df122009fd784e4191cb0299ba0e3ad0b1
Ronnie <epicbrownie@gmail.com>
2019-10-20 09:28:21 -0500

    refactor: formatting

--- src/Math/detail/Angle_impl.hpp
+++ src/Math/detail/Angle_impl.hpp
@@ -56,16 +56,16 @@ private:
 	value_type m_Value;
 
 public:
-	static const Radian<T> Zero;
-	static const Radian<T> QuarterCircle;
-	static const Radian<T> HalfCircle;
-	static const Radian<T> ThreeQuarterCircle;
-	static const Radian<T> Circle;
+	static const Radian Zero;
+	static const Radian QuarterCircle;
+	static const Radian HalfCircle;
+	static const Radian ThreeQuarterCircle;
+	static const Radian Circle;
 
 public:
 	constexpr Radian() noexcept = default;
-	constexpr Radian(const Radian<T>&) noexcept = default;
-	Radian(Radian<T>&&) noexcept = default;
+	constexpr Radian(const Radian&) noexcept = default;
+	Radian(Radian&&) noexcept = default;
 	~Radian() = default;
 
 	template<class U, typename = std::enable_if_t<!std::is_same_v<U, T> && std::is_convertible_v<U, T>>>
@@ -99,7 +99,7 @@ public:
 	auto SinCos() const noexcept { return std::make_pair(Sin(), Cos()); }
 
 public:
-	Radian<T>& Normalize() noexcept
+	Radian& Normalize() noexcept
 	{
 		m_Value = std::remainder(m_Value, Epic::TwoPi<T>);
 		m_Value += (m_Value < T(0)) ? Epic::TwoPi<T> : T(0);
@@ -107,105 +107,105 @@ public:
 		return *this;
 	}
 
-	static Radian<T> NormalOf(Radian<T> value) noexcept
+	static Radian NormalOf(Radian value) noexcept
 	{
 		return value.Normalize();
 	}
 
 public:
-	Radian<T>& operator = (Radian<T>&& value) = default;
+	Radian& operator = (Radian&& value) = default;
 
-	constexpr Radian<T> operator - () const { return { -m_Value }; }
+	constexpr Radian operator - () const { return { -m_Value }; }
 
-	constexpr Radian<T>& operator = (Radian<T> value) noexcept { m_Value = value.Value(); return *this; }
-	constexpr Radian<T>& operator += (Radian<T> value) noexcept { m_Value += value.Value(); return *this; }
-	constexpr Radian<T>& operator -= (Radian<T> value) noexcept { m_Value -= value.Value(); return *this; }
-	constexpr Radian<T>& operator *= (Radian<T> value) noexcept { m_Value *= value.Value(); return *this; }
-	constexpr Radian<T>& operator /= (Radian<T> value) noexcept { m_Value /= value.Value(); return *this; }
+	constexpr Radian& operator = (Radian value) noexcept { m_Value = value.Value(); return *this; }
+	constexpr Radian& operator += (Radian value) noexcept { m_Value += value.Value(); return *this; }
+	constexpr Radian& operator -= (Radian value) noexcept { m_Value -= value.Value(); return *this; }
+	constexpr Radian& operator *= (Radian value) noexcept { m_Value *= value.Value(); return *this; }
+	constexpr Radian& operator /= (Radian value) noexcept { m_Value /= value.Value(); return *this; }
 
-	constexpr Radian<T>& operator = (value_type value) noexcept { m_Value = value; return *this; }
-	constexpr Radian<T>& operator += (value_type value) noexcept { m_Value += value; return *this; }
-	constexpr Radian<T>& operator -= (value_type value) noexcept { m_Value -= value; return *this; }
-	constexpr Radian<T>& operator *= (value_type value) noexcept { m_Value *= value; return *this; }
-	constexpr Radian<T>& operator /= (value_type value) noexcept { m_Value /= value; return *this; }
+	constexpr Radian& operator = (value_type value) noexcept { m_Value = value; return *this; }
+	constexpr Radian& operator += (value_type value) noexcept { m_Value += value; return *this; }
+	constexpr Radian& operator -= (value_type value) noexcept { m_Value -= value; return *this; }
+	constexpr Radian& operator *= (value_type value) noexcept { m_Value *= value; return *this; }
+	constexpr Radian& operator /= (value_type value) noexcept { m_Value /= value; return *this; }
 
-	constexpr Radian<T>& operator = (Degree<T> value) noexcept
+	constexpr Radian& operator = (Degree<T> value) noexcept
 	{
 		m_Value = Epic::DegreesToRadians(value.Value());
 		return *this;
 	}
 
-	constexpr Radian<T>& operator += (Degree<T> value) noexcept
+	constexpr Radian& operator += (Degree<T> value) noexcept
 	{
 		m_Value += Epic::DegreesToRadians(value.Value());
 		return *this;
 	}
 
-	constexpr Radian<T>& operator -= (Degree<T> value) noexcept
+	constexpr Radian& operator -= (Degree<T> value) noexcept
 	{
 		m_Value -= Epic::DegreesToRadians(value.Value());
 		return *this;
 	}
 
-	constexpr Radian<T>& operator *= (Degree<T> value) noexcept
+	constexpr Radian& operator *= (Degree<T> value) noexcept
 	{
 		m_Value *= Epic::DegreesToRadians(value.Value());
 		return *this;
 	}
 
-	constexpr Radian<T>& operator /= (Degree<T> value) noexcept
+	constexpr Radian& operator /= (Degree<T> value) noexcept
 	{
 		m_Value /= Epic::DegreesToRadians(value.Value());
 		return *this;
 	}
 
 public:
-	constexpr Radian<T> operator + (Radian<T> value) const noexcept { return { m_Value + value.Value() }; }
-	constexpr Radian<T> operator - (Radian<T> value) const noexcept { return { m_Value - value.Value() }; }
-	constexpr Radian<T> operator * (Radian<T> value) const noexcept { return { m_Value * value.Value() }; }
-	constexpr Radian<T> operator / (Radian<T> value) const noexcept { return { m_Value / value.Value() }; }
+	constexpr Radian operator + (Radian value) const noexcept { return { m_Value + value.Value() }; }
+	constexpr Radian operator - (Radian value) const noexcept { return { m_Value - value.Value() }; }
+	constexpr Radian operator * (Radian value) const noexcept { return { m_Value * value.Value() }; }
+	constexpr Radian operator / (Radian value) const noexcept { return { m_Value / value.Value() }; }
 	 
-	constexpr Radian<T> operator + (value_type value) const noexcept { return { m_Value + value }; }
-	constexpr Radian<T> operator - (value_type value) const noexcept { return { m_Value - value }; }
-	constexpr Radian<T> operator * (value_type value) const noexcept { return { m_Value * value }; }
-	constexpr Radian<T> operator / (value_type value) const noexcept { return { m_Value / value }; }
+	constexpr Radian operator + (value_type value) const noexcept { return { m_Value + value }; }
+	constexpr Radian operator - (value_type value) const noexcept { return { m_Value - value }; }
+	constexpr Radian operator * (value_type value) const noexcept { return { m_Value * value }; }
+	constexpr Radian operator / (value_type value) const noexcept { return { m_Value / value }; }
 
-	constexpr Radian<T> operator + (Degree<T> value) const noexcept
+	constexpr Radian operator + (Degree<T> value) const noexcept
 	{
 		return { m_Value + Epic::DegreesToRadians(value.Value()) };
 	}
 
-	constexpr Radian<T> operator - (Degree<T> value) const noexcept
+	constexpr Radian operator - (Degree<T> value) const noexcept
 	{
 		return { m_Value - Epic::DegreesToRadians(value.Value()) };
 	}
 
-	constexpr Radian<T> operator * (Degree<T> value) const noexcept
+	constexpr Radian operator * (Degree<T> value) const noexcept
 	{
 		return { m_Value * Epic::DegreesToRadians(value.Value()) };
 	}
 
-	constexpr Radian<T> operator / (Degree<T> value) const noexcept
+	constexpr Radian operator / (Degree<T> value) const noexcept
 	{
 		return { m_Value / Epic::DegreesToRadians(value.Value()) };
 	}
 
-	friend constexpr Radian<T> operator + (value_type value, Radian<T> rad) noexcept
+	friend constexpr Radian operator + (value_type value, Radian rad) noexcept
 	{
 		return { value + rad.Value() };
 	}
 
-	friend constexpr Radian<T> operator - (value_type value, Radian<T> rad) noexcept
+	friend constexpr Radian operator - (value_type value, Radian rad) noexcept
 	{
 		return { value - rad.Value() };
 	}
 
-	friend constexpr Radian<T> operator * (value_type value, Radian<T> rad) noexcept
+	friend constexpr Radian operator * (value_type value, Radian rad) noexcept
 	{
 		return { value * rad.Value() };
 	}
 
-	friend constexpr Radian<T> operator / (value_type value, Radian<T> rad) noexcept
+	friend constexpr Radian operator / (value_type value, Radian rad) noexcept
 	{
 		return { value / rad.Value() };
 	}
@@ -218,12 +218,12 @@ public:
 	constexpr bool operator == (value_type value) const noexcept { return m_Value == value; }
 	constexpr bool operator != (value_type value) const noexcept { return m_Value != value; }
 
-	constexpr bool operator < (Radian<T> value) const noexcept { return m_Value < value.Value(); }
-	constexpr bool operator > (Radian<T> value) const noexcept { return m_Value > value.Value(); }
-	constexpr bool operator <= (Radian<T> value) const noexcept { return m_Value <= value.Value(); }
-	constexpr bool operator >= (Radian<T> value) const noexcept { return m_Value >= value.Value(); }
-	constexpr bool operator == (Radian<T> value) const noexcept { return m_Value == value.Value(); }
-	constexpr bool operator != (Radian<T> value) const noexcept { return m_Value != value.Value(); }
+	constexpr bool operator < (Radian value) const noexcept { return m_Value < value.Value(); }
+	constexpr bool operator > (Radian value) const noexcept { return m_Value > value.Value(); }
+	constexpr bool operator <= (Radian value) const noexcept { return m_Value <= value.Value(); }
+	constexpr bool operator >= (Radian value) const noexcept { return m_Value >= value.Value(); }
+	constexpr bool operator == (Radian value) const noexcept { return m_Value == value.Value(); }
+	constexpr bool operator != (Radian value) const noexcept { return m_Value != value.Value(); }
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -240,16 +240,16 @@ private:
 	value_type m_Value;
 
 public:
-	static const Degree<T> Zero;
-	static const Degree<T> QuarterCircle;
-	static const Degree<T> HalfCircle;
-	static const Degree<T> ThreeQuarterCircle;
-	static const Degree<T> Circle;
+	static const Degree Zero;
+	static const Degree QuarterCircle;
+	static const Degree HalfCircle;
+	static const Degree ThreeQuarterCircle;
+	static const Degree Circle;
 
 public:
 	constexpr Degree() noexcept = default;
-	constexpr Degree(const Degree<T>&) noexcept = default;
-	Degree(Degree<T>&&) noexcept = default;
+	constexpr Degree(const Degree&) noexcept = default;
+	Degree(Degree&&) noexcept = default;
 	~Degree() = default;
 
 	template<class U, typename = std::enable_if_t<!std::is_same_v<U, T> && std::is_convertible_v<U, T>>>
@@ -283,7 +283,7 @@ public:
 	auto SinCos() const noexcept { return std::make_pair(Sin(), Cos()); }
 
 public:
-	Degree<T>& Normalize() noexcept
+	Degree& Normalize() noexcept
 	{
 		m_Value = std::remainder(m_Value, T(360));
 		m_Value += (m_Value < T(0)) ? T(360) : T(0);
@@ -291,105 +291,105 @@ public:
 		return *this;
 	}
 
-	static Degree<T> NormalOf(Degree<T> value) noexcept
+	static Degree NormalOf(Degree value) noexcept
 	{
 		return value.Normalize();
 	}
 
 public:
-	Degree<T>& operator = (Degree<T>&& value) = default;
+	Degree& operator = (Degree&& value) = default;
 
-	constexpr Degree<T> operator - () const { return { -m_Value }; }
+	constexpr Degree operator - () const { return { -m_Value }; }
 
-	constexpr Degree<T>& operator = (Degree<T> value) noexcept { m_Value = value.Value(); return *this; }
-	constexpr Degree<T>& operator += (Degree<T> value) noexcept { m_Value += value.Value(); return *this; }
-	constexpr Degree<T>& operator -= (Degree<T> value) noexcept { m_Value -= value.Value(); return *this; }
-	constexpr Degree<T>& operator *= (Degree<T> value) noexcept { m_Value *= value.Value(); return *this; }
-	constexpr Degree<T>& operator /= (Degree<T> value) noexcept { m_Value /= value.Value(); return *this; }
+	constexpr Degree& operator = (Degree value) noexcept { m_Value = value.Value(); return *this; }
+	constexpr Degree& operator += (Degree value) noexcept { m_Value += value.Value(); return *this; }
+	constexpr Degree& operator -= (Degree value) noexcept { m_Value -= value.Value(); return *this; }
+	constexpr Degree& operator *= (Degree value) noexcept { m_Value *= value.Value(); return *this; }
+	constexpr Degree& operator /= (Degree value) noexcept { m_Value /= value.Value(); return *this; }
 
-	constexpr Degree<T>& operator = (value_type value) noexcept { m_Value = value; return *this; }
-	constexpr Degree<T>& operator += (value_type value) noexcept { m_Value += value; return *this; }
-	constexpr Degree<T>& operator -= (value_type value) noexcept { m_Value -= value; return *this; }
-	constexpr Degree<T>& operator *= (value_type value) noexcept { m_Value *= value; return *this; }
-	constexpr Degree<T>& operator /= (value_type value) noexcept { m_Value /= value; return *this; }
+	constexpr Degree& operator = (value_type value) noexcept { m_Value = value; return *this; }
+	constexpr Degree& operator += (value_type value) noexcept { m_Value += value; return *this; }
+	constexpr Degree& operator -= (value_type value) noexcept { m_Value -= value; return *this; }
+	constexpr Degree& operator *= (value_type value) noexcept { m_Value *= value; return *this; }
+	constexpr Degree& operator /= (value_type value) noexcept { m_Value /= value; return *this; }
 
-	constexpr Degree<T>& operator = (Radian<T> value) noexcept
+	constexpr Degree& operator = (Radian<T> value) noexcept
 	{
 		m_Value = Epic::RadiansToDegrees(value.Value());
 		return *this;
 	}
 
-	constexpr Degree<T>& operator += (Radian<T> value) noexcept
+	constexpr Degree& operator += (Radian<T> value) noexcept
 	{
 		m_Value += Epic::RadiansToDegrees(value.Value());
 		return *this;
 	}
 
-	constexpr Degree<T>& operator -= (Radian<T> value) noexcept
+	constexpr Degree& operator -= (Radian<T> value) noexcept
 	{
 		m_Value -= Epic::RadiansToDegrees(value.Value());
 		return *this;
 	}
 
-	constexpr Degree<T>& operator *= (Radian<T> value) noexcept
+	constexpr Degree& operator *= (Radian<T> value) noexcept
 	{
 		m_Value *= Epic::RadiansToDegrees(value.Value());
 		return *this;
 	}
 
-	constexpr Degree<T>& operator /= (Radian<T> value) noexcept
+	constexpr Degree& operator /= (Radian<T> value) noexcept
 	{
 		m_Value /= Epic::RadiansToDegrees(value.Value());
 		return *this;
 	}
 
 public:
-	constexpr Degree<T> operator + (Degree<T> value) const noexcept { return { m_Value + value.Value() }; }
-	constexpr Degree<T> operator - (Degree<T> value) const noexcept { return { m_Value - value.Value() }; }
-	constexpr Degree<T> operator * (Degree<T> value) const noexcept { return { m_Value * value.Value() }; }
-	constexpr Degree<T> operator / (Degree<T> value) const noexcept { return { m_Value / value.Value() }; }
+	constexpr Degree operator + (Degree value) const noexcept { return { m_Value + value.Value() }; }
+	constexpr Degree operator - (Degree value) const noexcept { return { m_Value - value.Value() }; }
+	constexpr Degree operator * (Degree value) const noexcept { return { m_Value * value.Value() }; }
+	constexpr Degree operator / (Degree value) const noexcept { return { m_Value / value.Value() }; }
 
-	constexpr Degree<T> operator + (value_type value) const noexcept { return { m_Value + value }; }
-	constexpr Degree<T> operator - (value_type value) const noexcept { return { m_Value - value }; }
-	constexpr Degree<T> operator * (value_type value) const noexcept { return { m_Value * value }; }
-	constexpr Degree<T> operator / (value_type value) const noexcept { return { m_Value / value }; }
+	constexpr Degree operator + (value_type value) const noexcept { return { m_Value + value }; }
+	constexpr Degree operator - (value_type value) const noexcept { return { m_Value - value }; }
+	constexpr Degree operator * (value_type value) const noexcept { return { m_Value * value }; }
+	constexpr Degree operator / (value_type value) const noexcept { return { m_Value / value }; }
 
-	constexpr Degree<T> operator + (Radian<T> value) const noexcept
+	constexpr Degree operator + (Radian<T> value) const noexcept
 	{
 		return { m_Value + Epic::RadiansToDegrees(value.Value()) };
 	}
 
-	constexpr Degree<T> operator - (Radian<T> value) const noexcept
+	constexpr Degree operator - (Radian<T> value) const noexcept
 	{
 		return { m_Value - Epic::RadiansToDegrees(value.Value()) };
 	}
 
-	constexpr Degree<T> operator * (Radian<T> value) const noexcept
+	constexpr Degree operator * (Radian<T> value) const noexcept
 	{
 		return { m_Value * Epic::RadiansToDegrees(value.Value()) };
 	}
 
-	constexpr Degree<T> operator / (Radian<T> value) const noexcept
+	constexpr Degree operator / (Radian<T> value) const noexcept
 	{
 		return { m_Value / Epic::RadiansToDegrees(value.Value()) };
 	}
 
-	friend constexpr Degree<T> operator + (value_type value, Degree<T> deg) noexcept
+	friend constexpr Degree operator + (value_type value, Degree deg) noexcept
 	{
 		return { value + deg.Value() };
 	}
 
-	friend constexpr Degree<T> operator - (value_type value, Degree<T> deg) noexcept
+	friend constexpr Degree operator - (value_type value, Degree deg) noexcept
 	{
 		return { value - deg.Value() };
 	}
 
-	friend constexpr Degree<T> operator * (value_type value, Degree<T> deg) noexcept
+	friend constexpr Degree operator * (value_type value, Degree deg) noexcept
 	{
 		return { value * deg.Value() };
 	}
 
-	friend constexpr Degree<T> operator / (value_type value, Degree<T> deg) noexcept
+	friend constexpr Degree operator / (value_type value, Degree deg) noexcept
 	{
 		return { value / deg.Value() };
 	}
@@ -402,12 +402,12 @@ public:
 	constexpr bool operator == (value_type value) const noexcept { return m_Value == value; }
 	constexpr bool operator != (value_type value) const noexcept { return m_Value != value; }
 
-	constexpr bool operator < (Degree<T> value) const noexcept { return m_Value < value.Value(); }
-	constexpr bool operator > (Degree<T> value) const noexcept { return m_Value > value.Value(); }
-	constexpr bool operator <= (Degree<T> value) const noexcept { return m_Value <= value.Value(); }
-	constexpr bool operator >= (Degree<T> value) const noexcept { return m_Value >= value.Value(); }
-	constexpr bool operator == (Degree<T> value) const noexcept { return m_Value == value.Value(); }
-	constexpr bool operator != (Degree<T> value) const noexcept { return m_Value != value.Value(); }
+	constexpr bool operator < (Degree value) const noexcept { return m_Value < value.Value(); }
+	constexpr bool operator > (Degree value) const noexcept { return m_Value > value.Value(); }
+	constexpr bool operator <= (Degree value) const noexcept { return m_Value <= value.Value(); }
+	constexpr bool operator >= (Degree value) const noexcept { return m_Value >= value.Value(); }
+	constexpr bool operator == (Degree value) const noexcept { return m_Value == value.Value(); }
+	constexpr bool operator != (Degree value) const noexcept { return m_Value != value.Value(); }
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -425,7 +425,7 @@ namespace Epic
 	const Radian<T> Radian<T>::HalfCircle = Epic::Pi<T>;
 
 	template<class T>
-	const Radian<T> Radian<T>::ThreeQuarterCircle = Epic::Pi<T> +Epic::HalfPi<T>;
+	const Radian<T> Radian<T>::ThreeQuarterCircle = Epic::Pi<T> + Epic::HalfPi<T>;
 
 	template<class T>
 	const Radian<T> Radian<T>::Circle = Epic::TwoPi<T>;
